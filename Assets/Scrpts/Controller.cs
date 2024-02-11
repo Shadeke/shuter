d64b3afa70c20f05da98682618c036e838d64da2
Shadeke <Vasaalekseev778@gmail.com>
2024-02-11 17:35:50 +0300

    1 Jump

---
+++ Assets/Scrpts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float Graviyty = 9.8f;
+    public float JumpForce;
+    private float _fallSpead = 0;
+    private CharacterController _CC;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _CC = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _CC.isGrounded)
+        {
+            _fallSpead = -JumpForce;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _fallSpead += Graviyty * Time.fixedDeltaTime;
+        _CC.Move(Vector3.down * _fallSpead * Time.fixedDeltaTime);
+    }
+}
