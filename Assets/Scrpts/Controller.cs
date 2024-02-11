266ab39e6e62e1cee2dcfb31d28d26cdaf2805b1
Shadeke <Vasaalekseev778@gmail.com>
2024-02-11 21:12:54 +0300

    ДЗ.1.3

--- Assets/Scrpts/Controller.cs
+++ Assets/Scrpts/Controller.cs
@@ -6,8 +6,11 @@ public class Controller : MonoBehaviour
 {
     public float Graviyty = 9.8f;
     public float JumpForce;
+    public float Speed;
     private float _fallSpead = 0;
     private CharacterController _CC;
+    private Vector3 _vector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,25 @@ public class Controller : MonoBehaviour
         {
             _fallSpead = -JumpForce;
         }
+        Debug.Log(_CC.isGrounded);
+        //бег
+        _vector = Vector3.zero;
+        if(Input.GetKey(KeyCode.W))
+        {
+            _vector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _vector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _vector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _vector -= transform.right;
+        }
 
     }
 
@@ -27,6 +49,15 @@ public class Controller : MonoBehaviour
     void FixedUpdate()
     {
         _fallSpead += Graviyty * Time.fixedDeltaTime;
+
+        if (_CC.isGrounded)
+        {
+            _fallSpead = 0;
+        }
         _CC.Move(Vector3.down * _fallSpead * Time.fixedDeltaTime);
+
+
+        // бег
+        _CC.Move(_vector * Speed * Time.fixedDeltaTime);
     }
-}
+}   
